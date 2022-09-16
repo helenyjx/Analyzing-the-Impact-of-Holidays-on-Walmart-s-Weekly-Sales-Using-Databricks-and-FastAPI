261a721896def777dacee368eaebbd9d07c486dc
helenyjx <112274822+helenyjx@users.noreply.github.com>
2022-09-16 21:06:40 +0000

    submit

--- fastapi_app.py
+++ fastapi_app.py
@@ -6,11 +6,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, welcome to Databricks from Walmart, lets's begin to research weekly sales from holidays and non-holidays!"}
+    return {"message": "Hello, welcome to Databricks from Walmart, lets's begin to research weekly sales from weeks include holidays and non-holidays!"}
 
 @app.get("/query")
 async def query():
-    """Execute a SQL query from Walmart database to search weekly sales of holidays and non-hoildays"""
+    """Execute a SQL query from Walmart database to search weekly sales that includes holidays and non-hoildays"""
 
     result = querydb()
     return {"Walmart weekly sales": result}
